937f6de3c471ba6c7115f741a0fdc6702f31b09a
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2023-11-30 17:59:15 +0100

    Add length conversion functionality

--- feet-to-meters/main.py
+++ feet-to-meters/main.py
@@ -1,6 +1,26 @@
 from tkinter import *
 from tkinter import ttk
 
+
+def calculate(*args):
+    try:
+        length1 = float(length1_entry.get())
+
+        converted_length = {
+            "meter": length1 * 0.3048,
+            "kilometer": length1 * 0.0003048,
+            "decimeter": length1 * 3.048,
+            "centimeter": length1 * 30.48,
+            "millimeter": length1 * 304.8,
+            "feet": length1,
+        }[to_unit_combobox.get()]
+
+        length2.set(converted_length)
+
+    except ValueError:
+        pass
+
+
 root = Tk()
 root.title("Length Converter")
 
@@ -14,19 +34,20 @@ length1_entry = ttk.Entry(main_frame, width=7, textvariable=length1)
 length1_entry.grid(column=2, row=1, sticky=(W, E))
 
 length2 = StringVar()
-ttk.Entry(main_frame, width=7, textvariable=length2).grid(
-    column=2, row=2, sticky=(W, E)
-)
+length2_entry = ttk.Entry(main_frame, width=7, textvariable=length2)
+length2_entry.grid(column=2, row=2, sticky=(W, E))
+length2_entry.configure(state="readonly")
 
 ttk.Button(main_frame, text="Calculate").grid(column=3, row=3, sticky=W)
 
-from_unit_combobox = ttk.Combobox(
-    main_frame,
-    width=7,
-    values=["meter", "kilometer", "decimeter", "centimeter", "millimeter", "feet"],
-)
-from_unit_combobox.grid(column=3, row=1, sticky=W)
-from_unit_combobox.current(5)
+# from_unit_combobox = ttk.Combobox(
+#     main_frame,
+#     width=7,
+#     values=["meter", "kilometer", "decimeter", "centimeter", "millimeter", "feet"],
+# )
+# from_unit_combobox.grid(column=3, row=1, sticky=W)
+# from_unit_combobox.current(5)
+ttk.Label(main_frame, text="feet").grid(column=3, row=1, sticky=W)
 
 ttk.Label(main_frame, text="is equivalent to").grid(column=1, row=2, sticky=E)
 
@@ -42,6 +63,6 @@ for child in main_frame.winfo_children():
     child.grid_configure(padx=5, pady=5)
 
 length1_entry.focus()
-# root.bind("<Return>", )
+root.bind("<Return>", calculate)
 
 mainloop()
