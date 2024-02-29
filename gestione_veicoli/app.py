7b7c9c18807dfa0b906285476911d058da142ba0
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-02-29 18:01:33 +0100

    Created Autoveicolo class

--- gestione_veicoli/app.py
+++ gestione_veicoli/app.py
@@ -1,4 +1,5 @@
 from classes.autocarro import Autocarro
+from classes.autoveicolo import Autoveicolo
 
 autocarro = Autocarro("3487857", "Tesla", "Cybertruck", 5, 50000, 4)
 print(autocarro.get_prezzo())
@@ -6,3 +7,10 @@ print(autocarro.get_marca())
 print(autocarro.get_max_carico())
 print(autocarro.get_modello())
 print(autocarro.get_numero_posti())
+
+autoveicolo = Autoveicolo("3433434", "Tesla", "Model S", 5, 15000, 5)
+print(autoveicolo.get_prezzo())
+print(autoveicolo.get_marca())
+print(autoveicolo.get_numero_porte())
+print(autoveicolo.get_modello())
+print(autoveicolo.get_numero_posti())
---
+++ gestione_veicoli/classes/autoveicolo.py
@@ -0,0 +1,30 @@
+from classes.veicolo import Veicolo
+
+
+class Autoveicolo(Veicolo):
+    def __init__(
+        self,
+        targa: str,
+        marca: str,
+        modello: str,
+        n_posti: int,
+        prezzo_base: float,
+        numero_porte: int,
+    ) -> None:
+        super().__init__(targa, marca, modello, n_posti, prezzo_base)
+        self.set_numero_porte(numero_porte)
+
+    def __str__(self) -> str:
+        return super().__str__()
+
+    def set_numero_porte(self, numero_porte: int):
+        if numero_porte <= 0:
+            raise ValueError("Il numero di porte deve essere maggiore di zero")
+
+        self.__numero_porte = numero_porte
+
+    def get_numero_porte(self):
+        return self.__numero_porte
+
+    def get_prezzo(self):
+        return self.get_prezzo_base() * self.get_numero_porte()
