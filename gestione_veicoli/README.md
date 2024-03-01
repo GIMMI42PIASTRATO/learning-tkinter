612a5a2885fa8ae72166f62cc69e356514c82278
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-03-01 17:01:21 +0100

    Add Concessionario class with methods for managing vehicles and README.md

---
+++ gestione_veicoli/README.md
@@ -0,0 +1,6 @@
+# ATTENZIONE
+Questo programma utilizza la sintassi di typing di python 3.10+, per eseguire il programma è richiesto avere quindi una versione di python 3.10 o superiore.
+
+### Typing
+Permette di specificare i type alle variabili, parametri e valori di ritorno delle funzioni.<br>
+Questi type sono *"finti"*, perché non danno nessun errore runtime se non vengono rispettati, ma permettono uno sviluppo più intuitivo.
---
+++ gestione_veicoli/classes/concessionario.py
@@ -0,0 +1,142 @@
+from classes.autocarro import Autocarro
+from classes.autoveicolo import Autoveicolo
+from classes.motoveicolo import Motoveicolo
+
+
+class Concessionario:
+    def __init__(
+        self,
+        nome: str,
+        indirizzo: str,
+        citta: str,
+        cap: int,
+        provincia: str,
+        telefono: str,
+        email: str,
+    ) -> None:
+        self.set_nome(nome)
+        self.set_indirizzo(indirizzo)
+        self.set_citta(citta)
+        self.set_cap(cap)
+        self.set_provincia(provincia)
+        self.set_telefono(telefono)
+        self.set_email(email)
+        self.__veicoli = []
+
+    def __str__(self) -> str:
+        return f"{self.__nome} - {self.__citta} ({self.__provincia}) - {self.__telefono} - {self.__email} - {len(self.__veicoli)} veicoli"
+
+    def get_nome(self):
+        return self.__nome
+
+    def get_indirizzo(self):
+        return self.__indirizzo
+
+    def get_citta(self):
+        return self.__citta
+
+    def get_cap(self):
+        return self.__cap
+
+    def get_provincia(self):
+        return self.__provincia
+
+    def get_telefono(self):
+        return self.__telefono
+
+    def get_email(self):
+        return self.__email
+
+    def get_veicoli(self):
+        return self.__veicoli
+
+    def set_nome(self, nome: str):
+        self.__nome = nome
+
+    def set_indirizzo(self, indirizzo: str):
+        self.__indirizzo = indirizzo
+
+    def set_citta(self, citta: str):
+        self.__citta = citta
+
+    def set_cap(self, cap: int):
+        self.__cap = cap
+
+    def set_provincia(self, provincia: str):
+        self.__provincia = provincia
+
+    def set_telefono(self, telefono: str):
+        self.__telefono = telefono
+
+    def set_email(self, email: str):
+        self.__email = email
+
+    def set_veicoli(self, veicoli: list["Autocarro" | "Autoveicolo" | "Motoveicolo"]):
+        self.__veicoli = veicoli
+
+    # Metodi gestione Veicoli
+    def stampa_veicoli(self):
+        # TODO implementa il metodo per funzionare sulla GUI
+        for veicolo in self.__veicoli:
+            print(veicolo)
+
+    def stampa_numero_veicoli(self):
+        # TODO implementa il metodo per funzionare sulla GUI
+        print(len(self.__veicoli))
+
+    # Metodi gestione Autoveicoli
+    def stampa_autoveicoli(self):
+        # TODO implementa il metodo per funzionare sulla GUI
+        for veicolo in self.__veicoli:
+            if isinstance(veicolo, Autoveicolo):
+                print(veicolo)
+
+    def stampa_numero_autoveicoli(self):
+        # TODO implementa il metodo per funzionare sulla GUI
+        count = 0
+        for veicolo in self.__veicoli:
+            if isinstance(veicolo, Autoveicolo):
+                count += 1
+        print(count)
+
+    # Metodi gestione Autocarri
+    def stampa_autocarri(self):
+        # TODO implementa il metodo per funzionare sulla GUI
+        for veicolo in self.__veicoli:
+            if isinstance(veicolo, Autocarro):
+                print(veicolo)
+
+    def stampa_numero_autocarri(self):
+        # TODO implementa il metodo per funzionare sulla GUI
+        count = 0
+        for veicolo in self.__veicoli:
+            if isinstance(veicolo, Autocarro):
+                count += 1
+        print(count)
+
+    # Metodi gestione Motoveicoli
+    def stampa_motoveicoli(self):
+        # TODO implementa il metodo per funzionare sulla GUI
+        for veicolo in self.__veicoli:
+            if isinstance(veicolo, Motoveicolo):
+                print(veicolo)
+
+    def stampa_numero_motoveicoli(self):
+        # TODO implementa il metodo per funzionare sulla GUI
+        count = 0
+        for veicolo in self.__veicoli:
+            if isinstance(veicolo, Motoveicolo):
+                count += 1
+        print(count)
+
+    # Stampa veicolo data la targa
+    def stampa_veicolo(self, targa: str):
+        # TODO implementa il metodo per funzionare sulla GUI
+        veicolo_trovato = False
+        for veicolo in self.__veicoli:
+            if veicolo.get_targa() == targa:
+                print(veicolo)
+                veicolo_trovato = True
+
+        if not veicolo_trovato:
+            print(f"Veicolo non trovato con targa: {targa}")
